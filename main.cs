a061a0aa2b21291310de7134535409eb7ec5626a
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-01 19:59:41 +0200

    push

--- main.cs
+++ main.cs
@@ -1,17 +1,15 @@
+using System
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld /* defines namespace. Namespace is a collectional grouping of classes. HelloWorld is defined as namespace name */
-{ 
-    class Program /* defines a class */
+namespace Cylinder Calc
+{
+    class Program
     {
-        static void Main(string[]args) /* main is entry point of programm. Static links the method to the class it is indented in. Void means it does not return any value by itself. Curly braces are the body of the method. */
+        static void Main(string[] args)
         {
-            COnsole.WriteLine("Hello World")
+            System.Console.WriteLine("Hello! This is a test program for calculating the Volumen of a cylinder");
         }
     }
 }
-
-/* defines Classes, Methods and namespaces */
